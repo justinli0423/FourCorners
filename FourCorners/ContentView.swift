9846f9887e6c4618971f9b784826a16955314134
Justin Li <justin.li0423@gmail.com>
2021-11-13 15:19:48 -0500

    Initial Commit

---
+++ FourCorners/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FourCorners
+//
+//  Created by Justin Li on 2021-11-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FourCorners/FourCornersApp.swift
@@ -0,0 +1,17 @@
+//
+//  FourCornersApp.swift
+//  FourCorners
+//
+//  Created by Justin Li on 2021-11-13.
+//
+
+import SwiftUI
+
+@main
+struct FourCornersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
